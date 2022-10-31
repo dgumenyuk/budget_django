18a35bf1704e8e4b43a56727184aa77368166667
dgumenyuk <gumenyuk98@gmail.com>
2022-10-31 13:50:36 -0400

    changes

--- script.py
+++ script.py
@@ -5,7 +5,4 @@ import subprocess
 #subprocess.run("git bisect run python manage.py test")
 #subprocess.run("git bisect reset")
 
-subprocess.run("git bisect start")
-subprocess.run("git bisect bad HEAD")
-subprocess.run("git bisect good e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c")
-subprocess.run("git bisect reset")
+subprocess.run("pwd")
