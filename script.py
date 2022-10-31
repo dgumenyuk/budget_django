d1612a677a7dea9703cfcff626559f0ad994853b
dgumenyuk <gumenyuk98@gmail.com>
2022-10-31 14:14:48 -0400

    tttt

--- script.py
+++ script.py
@@ -12,6 +12,6 @@ import os
 
 os.system("git bisect start")
 os.system("git bisect bad HEAD")
-os.system("git bisect good e4cfc6f")
+os.system("git bisect good HEAD~10")
 os.system("git bisect run python manage.py test")
 os.system("git bisect reset")
