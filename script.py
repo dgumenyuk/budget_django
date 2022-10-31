29192be52908f5936c957ef889e31a6036d8f119
dgumenyuk <gumenyuk98@gmail.com>
2022-10-31 13:28:09 -0400

    Adding workflow

---
+++ script.py
@@ -0,0 +1,6 @@
+import subprocess
+
+
+subprocess.run("git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c")
+subprocess.run("git bisect run python manage.py test")
+subprocess.run("git bisect reset")
