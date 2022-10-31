4be28cafd058b0877b677946c09bf3bb760f680e
dgumenyuk <gumenyuk98@gmail.com>
2022-10-31 15:56:30 -0400

    new change

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-
+'''
 import subprocess
 
 
@@ -7,11 +7,11 @@ subprocess.run("git bisect run python manage.py test")
 subprocess.run("git bisect reset")
 
 #subprocess.run("pwd")
+'''
+import os
 
-#import os
-
-#os.system("git bisect start")
-#os.system("git bisect bad c1a4be04b972b6c17db242fc37752ad517c29402")
-#os.system("git bisect good e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c")
-#os.system("git bisect run python manage.py test")
-#os.system("git bisect reset")
+os.system("git bisect start")
+os.system("git bisect bad c1a4be04b972b6c17db242fc37752ad517c29402")
+os.system("git bisect good e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c")
+os.system("git bisect run python manage.py test")
+os.system("git bisect reset")
